f2ddaed44f5fcc7b0eba1ce05fa69b17fe1d14c7
lyn1878 <ansellin001@gmail.com>
2024-07-26 10:58:50 +0800

    Update content

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: {
+          showReadingTime: true,
+          postsPerPage: 'ALL', // This setting ensures all blog posts are displayed
+          blogSidebarCount: 'ALL', // Ensures all posts are displayed in the sidebar
+          blogSidebarTitle: 'All Blog Posts',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
 };
